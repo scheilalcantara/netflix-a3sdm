b7b4fc4aa341230a84b8f232b5510d290436960d
Scheila Alcântara <scheilalcantara2207@gmail.com>
2024-11-14 22:42:25 -0300

    first commit

---
+++ back/src/main/java/com/scheila/netflix_a3sdm/NetflixA3sdmApplication.java
@@ -0,0 +1,13 @@
+package com.scheila.netflix_a3sdm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetflixA3sdmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetflixA3sdmApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/com/scheila/netflix_a3sdm/NetflixA3sdmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scheila.netflix_a3sdm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixA3sdmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
